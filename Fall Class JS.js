225cd89fe4e232f4ca53233216c45af1ab67dd08
Fair-Game-TM <157636823+FAIR-GAME-TM@users.noreply.github.com>
2024-10-01 19:17:23 -0700

    JAVASCRIPT ASSIGNMENT
    I uploaded the files

---
+++ Fall Class JS.js
@@ -0,0 +1,5 @@
+function myFunction(){
+    document.getElementById("Javascript").innerHTML = "You Loose";
+    alert("You did not win, and this is my first time coding, thank you prof battana and levi")
+}
+
